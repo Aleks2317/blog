a049a932932d7c085223b9d9fc74319069026591
Aleksei Kavko <108331145+Aleks2317@users.noreply.github.com>
2024-08-05 19:59:31 +0300

    urls and re

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,39 @@
-from django.shortcuts import HttpResponse
+# from django.shortcuts import HttpResponse
+#
+#
+# def index(request):
+#     return HttpResponse('<h2>Главная<h2>')
+#
+#
+# def about(requests, name, age):
+#     return HttpResponse(f'''
+#     <h2>О пользователе</h2>
+#     <p>Имя: {name}</p>
+#     <p>Возраст: {age}</p>
+# ''')
+#
+#
+# def contact(request):
+#     return HttpResponse('<h2>Контакты</h2>')
+#
+#
+# def user(request, name='Undefined', age=0):
+#     return HttpResponse(f'<h2>Имя: {name}, Возраст: {age}</h2>')
+
+from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('<h2>Главная<h2>')
+    return HttpResponse("Главная страница")
 
 
-def about(requests):
-    return HttpResponse('<h2>О сайте<h2>')
+def products(request, id):
+    return HttpResponse(f"Товар {id}")
 
 
-def contact(request):
-    return HttpResponse('<h2>Контакты</h2>')
+def comments(request, id):
+    return HttpResponse(f"Комментарии о товаре {id}")
 
+
+def questions(request, id):
+    return HttpResponse(f"Вопросы о товаре {id}")
