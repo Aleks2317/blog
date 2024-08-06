3309103ab6867cb6501a6f4fc0ce42e4b1eac2b2
Aleksei Kavko <108331145+Aleks2317@users.noreply.github.com>
2024-08-06 11:01:21 +0300

    next urls paterns

--- blog/views.py
+++ blog/views.py
@@ -20,6 +20,24 @@
 # def user(request, name='Undefined', age=0):
 #     return HttpResponse(f'<h2>Имя: {name}, Возраст: {age}</h2>')
 
+# from django.http import HttpResponse
+#
+#
+# def index(request):
+#     return HttpResponse("Главная страница")
+#
+#
+# def products(request, id):
+#     return HttpResponse(f"Товар {id}")
+#
+#
+# def comments(request, id):
+#     return HttpResponse(f"Комментарии о товаре {id}")
+#
+#
+# def questions(request, id):
+#     return HttpResponse(f"Вопросы о товаре {id}")
+
 from django.http import HttpResponse
 
 
