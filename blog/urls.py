ef02304d8eb2643989d95322e529be4ed46533da
Aleksei Kavko <108331145+Aleks2317@users.noreply.github.com>
2024-08-08 14:05:06 +0300

    templates, base, include, extends

--- blog/urls.py
+++ blog/urls.py
@@ -1,29 +1,7 @@
-# from django.urls import path, re_path
-# from blog import views
-#
-# urlpatterns = [
-#     path('', views.index),
-#     path('about/', views.about, kwargs={'name': 'Tom', 'age': 38}),
-#     re_path(r'^contact/', views.contact),
-#     re_path(r'^user/(?P<name>\D+)/(?P<age>\d+)/', views.user),
-#     re_path(r'^user/(?P<name>\D+)/', views.user),
-#     re_path(r'^user/', views.user),
-# ]
-
-# from django.urls import path
-# from blog import views
-#
-# urlpatterns = [
-#     path("", views.index),
-#     path("about/", views.about),
-#     path("contact/", views.contact),
-#     path("details/", views.details),
-# ]
-
 from django.urls import path
 from blog import views
 
 urlpatterns = [
-    path("index/<int:id>/", views.index),
-    path("access/<int:age>/", views.access),
+    path("", views.index),
+    path("about/", views.about),
 ]
