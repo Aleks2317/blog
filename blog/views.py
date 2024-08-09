9884582a8845195cadc073e2cee10d8c0061882b
Aleksei Kavko <108331145+Aleks2317@users.noreply.github.com>
2024-08-09 12:12:03 +0300

    include

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, "blog/index.html")
+    return render(request, "blog/index.html", context={"site": "Stepik.org"})
 
 
 def about(request):
-    return render(request, "blog/about.html")
+    return render(request, "blog/about.html", context={"site": "Stepik"})
